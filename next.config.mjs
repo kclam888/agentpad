fab0677143566be932017051963e89df51c891ad
Your Name <your.email@example.com>
2025-02-13 01:42:53 +0800

    Revert to working Netlify configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -9,14 +10,10 @@ const nextConfig = {
         pathname: '/**',
       },
     ],
-    unoptimized: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  output: 'export',
-  distDir: 'out',
-  trailingSlash: true,
 };
 
 export default nextConfig; 
